83f695e219d044822fa0e1117bcb418b9fac582b
Max-vecna <maxsuelsoarescustodio@gmail.com>
2026-06-03 16:11:38 -0300

    Delete attack_renderer.js

--- attack_renderer.js
+++
@@ -1,6 +0,0 @@
-import { renderFullSpellSheet } from './magic_renderer.js';
-
-export async function renderFullAttackSheet(attackData, isModal, options = {}) {
-    if (!attackData) return '';
-    return renderFullSpellSheet({ ...attackData, type: 'ataque' }, isModal, options);
-}
